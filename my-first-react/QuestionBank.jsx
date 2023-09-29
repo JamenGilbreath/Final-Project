fdc44828599c41c8c1feea8bf97cf6c7c207c32d
JamenGilbreath <jamen.gilbreath.cohort233@gmail.com>
2023-09-29 16:53:57 -0700

    update 3 9/29

--- my-first-react/QuestionBank.jsx
+++ my-first-react/QuestionBank.jsx
@@ -1,35 +1,51 @@
-const qBank = [
-    
-    {
-        id: 1,
-        question: "What is the capital of Haryana?",
-        options: ["Yamunanagar", "Panipat", "Gurgaon", "Chandigarh"],
-        answer: "Chandigarh",
-    },
-    {
-        id: 2,
-        question: "What is the capital of Punjab?",
-        options: ["Patiala", "Ludhiana", "Amritsar", "Chandigarh"],
-        answer: "Chandigarh",
-    },
-    {
-        id: 3,
-        question: "What is the capital of India?",
-        options: ["Delhi", "Mumbai", "Kolkata", "Chennai"],
-        answer: "Delhi"
-    },
-    {
-        id: 4,
-        question: "What is the capital of Uttarakhad?",
-        options: ["Roorkee", "Haridwar", "Dehradun", "Nanital"],
-        answer: "Dehradun"
-    },
-    {
-        id: 5,
-        question: "What is capital of Uttar Pradesh?",
-        options: ["GB Nagar", "Lucknow", "Prayagraj", "Agra"],
-        answer: "Lucknow"
-    },
-]
- 
-export default qBank;
+import React, { Component } from "react";
+import axios from "axios"; 
+
+class QuestionBank extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      questionBank: [],
+      isLoading: true,
+    };
+  }
+
+  componentDidMount() {
+    axios
+      .get("https://opentdb.com/api.php?amount=10")
+      .then((response) => {
+        const questions = response.data.results;
+        const formattedQuestions = questions.map((question, index) => ({
+          id: index + 1,
+          question: question.question,
+          options: [...question.incorrect_answers, question.correct_answer],
+          answer: question.correct_answer,
+        }));
+        this.setState({ questionBank: formattedQuestions, isLoading: false });
+      })
+      .catch((error) => {
+        console.error("Error fetching questions:", error);
+        this.setState({ isLoading: false });
+      });
+  }
+
+  render() {
+    const { questionBank, isLoading } = this.state;
+    if (isLoading) {
+      return <div>Loading questions...</div>;
+    } else {
+      return (
+        <div>
+          {questionBank.map((question) => (
+            <div key={question.id}>
+              <h3>Question {question.id}</h3>
+              <p>{question.question}</p>
+            </div>
+          ))}
+        </div>
+      );
+    }
+  }
+}
+
+export default QuestionBank;
